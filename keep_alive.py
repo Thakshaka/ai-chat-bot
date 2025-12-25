73686814dfc700b089012ea469cb7b5af1256096
Thakshaka Rathnayake <thakshakarathnayake15@gmail.com>
2025-12-25 20:50:43 +0530

    Add health endpoint

--- keep_alive.py
+++ keep_alive.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask
 from threading import Thread
 
 app = Flask(__name__)
@@ -6,6 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Alive"
+
+@app.route('/healthz')
+def healthz():
+    return "OK"
 
 def run():
   app.run(host='0.0.0.0',port=8000)
